62e499bbf9aa0df04adc78d62b27f2b5b8a7c884
HamzHajra <hh51489@ubt-uni.net>
2022-04-24 19:14:04 +0200

    commit AboutUS

---
+++ AboutUs.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+  <head>
+<title>About Us</title>
+
+<link rel="stylesheet" type="text/css" href="css/style4.css">
+
+  </head>
+
+<body>
+    
+    <?php include("include/header.php")?>
+
+
+
+    <div class="about-section">
+        <h1>About Us Page</h1>
+        <p>Shop 14TECH for electronics, computers, cell phones, video games & more new tech.</p>
+        <p>14TECH offers the best prices on computer products, laptop computers, TV's. Laptops & computers. Gaming PC. Drones & Toys. TV & Audio. Brands: MSI, Apple, Samsung.</p>
+      </div>
+      
+      <h2 style="text-align:center; color: white;">Our Team</h2>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <img src="foto/team.png" alt="" style="width:100%">
+            <div class="container">
+              <h2>Qendrim Rexha</h2>
+              <p class="title">CEO & Founder</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>QRexha@gmail.com</p>
+              <p><a href="Contact.php"><button class="button">Contact</button></a></p>
+            </div>
+          </div>
+        </div>
+      
+        <div class="column">
+          <div class="card">
+            <img src="foto/team.png" alt="" style="width:100%">
+            <div class="container">
+              <h2>Hamz Hajra</h2>
+              <p class="title">Ceo & Founder</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>HHajra@gmail.com</p>
+              <p><a href="Contact.php"><button class="button">Contact</button></a></p>
+            </div>
+          </div>
+        </div>
+      
+      </div>
+      
+
+      <?php include("include/footer.php") ?>
+
+</body>
+</html>
