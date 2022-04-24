e5ebbe3786ad5ac405b24110e571546068a064b9
HamzHajra <hh51489@ubt-uni.net>
2022-04-24 19:17:28 +0200

    commit index

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<title>Index</title>
+<link rel="stylesheet" type="text/css" href="css/style7">
+
+
+    </head>
+<body>
+
+<?php include("include/header.php")?>
+
+          <div>
+              <div>
+                  <div>
+                      <div>
+                          <p>
+                              <?php
+
+                              // if(isset($_SESSION['message'])){
+                              // echo $_SESSION['message']; 
+                              // }
+
+                              ?>
+                          </p>
+                      </div>
+
+
+                      <?php
+                          
+                          // if(!isset($_SESSION['klienti'])){
+                              include "Login.php";
+                          // }
+
+                      ?>
+
+                  </div>
+              </div>
+          </div>
+
+    
+
+<?php include("include/footer.php") ?>
+
+
+ 
+</body>
+</html>
