bd81f05daa02d37eafba376c1d41c8639877b895
Qendrim14 <qr38352@ubt-uni.net>
2022-04-24 18:59:49 +0200

    commit Include

---
+++ include/footer.php
@@ -0,0 +1,8 @@
+
+<center> 
+    <div id="footer">
+    <img src="foto/../facebook.png" class="facebook">
+    <img src="foto/../instagram.png" class="instagram">
+    <img src="foto/../copyright.png" class="copyright">
+    </div>
+</center>
